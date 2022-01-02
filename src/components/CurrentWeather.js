9afce12c088184c3d157679cb00f6183c6f402ce
rustam-h <rustam.habibulla@gmail.com>
2022-01-02 17:09:38 +0400

    Separated background images array

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -3,21 +3,22 @@ import { Paper, Typography } from '@mui/material';
 import axios from 'axios';
 import CardCurrentWeather from './CardCurrentWeather';
 import card from '../static/card-bg.jpg'
-import ash from '../static/ash-bg.jpg'
-import clear from '../static/clear-bg.jpg'
-import clouds from '../static/clouds-bg.jpg'
-import drizzle from '../static/drizzle-bg.jpg'
-import dust from '../static/dust-bg.jpg'
-import fog from '../static/fog-bg.jpg'
-import haze from '../static/haze-bg.jpg'
-import mist from '../static/mist-bg.jpg'
-import rain from '../static/rain-bg.jpg'
-import sand from '../static/sand-bg.jpg'
-import smoke from '../static/smoke-bg.jpg'
-import snow from '../static/snow-bg.jpg'
-import squall from '../static/squall-bg.jpg'
-import thunderstorm from '../static/thunderstorm-bg.jpg'
-import tornado from '../static/tornado-bg.jpg'
+// import ash from '../static/ash-bg.jpg'
+// import clear from '../static/clear-bg.jpg'
+// import clouds from '../static/clouds-bg.jpg'
+// import drizzle from '../static/drizzle-bg.jpg'
+// import dust from '../static/dust-bg.jpg'
+// import fog from '../static/fog-bg.jpg'
+// import haze from '../static/haze-bg.jpg'
+// import mist from '../static/mist-bg.jpg'
+// import rain from '../static/rain-bg.jpg'
+// import sand from '../static/sand-bg.jpg'
+// import smoke from '../static/smoke-bg.jpg'
+// import snow from '../static/snow-bg.jpg'
+// import squall from '../static/squall-bg.jpg'
+// import thunderstorm from '../static/thunderstorm-bg.jpg'
+// import tornado from '../static/tornado-bg.jpg'
+import images from '../static/imagesList';
 
 function CurrentWeather(props) {
     const [weatherData, setWeatherData] = useState({});
@@ -41,33 +42,35 @@ function CurrentWeather(props) {
         console.log(weatherData.weather[0].main)
         bg_image_name = weatherData.weather[0].main.toLowerCase();
     }
-    let images = {
-        'ash': ash,
-        'clear': clear,
-        'clouds': clouds,
-        'drizzle': drizzle,
-        'dust': dust,
-        'fog': fog,
-        'haze': haze,
-        'mist': mist, 
-        'rain': rain,
-        'sand': sand,
-        'smoke': smoke,
-        'snow': snow,
-        'squall': squall,
-        'thunderstorm': thunderstorm,
-        'tornado': tornado
-    };
-    bg_image = images[bg_image_name];
+    // let images = {
+    //     'ash': ash,
+    //     'clear': clear,
+    //     'clouds': clouds,
+    //     'drizzle': drizzle,
+    //     'dust': dust,
+    //     'fog': fog,
+    //     'haze': haze,
+    //     'mist': mist, 
+    //     'rain': rain,
+    //     'sand': sand,
+    //     'smoke': smoke,
+    //     'snow': snow,
+    //     'squall': squall,
+    //     'thunderstorm': thunderstorm,
+    //     'tornado': tornado
+    // };
     console.log(bg_image);
+    if(bg_image_name){
+        bg_image = images[bg_image_name];
+    }
     return (
         <>
         { (weatherData.weather) ?
         <Paper 
         elevation={0}
+        square='true'
         sx={{ 
-            backgroundImage: "url(" + `${bg_image}` + ")",
-
+            backgroundImage: `url(${bg_image})`,
             backgroundPosition: 'center',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
@@ -80,7 +83,11 @@ function CurrentWeather(props) {
             null
             }
         </Paper>
-        :   <Typography>
+        :   <Typography 
+            variant="h4"
+            sx= {{
+                mt: 5
+            }}>
             Enter a city name to display its current weather
             </Typography>
     }
---
+++ src/static/imagesList.js
@@ -0,0 +1,35 @@
+import ash from '../static/ash-bg.jpg'
+import clear from '../static/clear-bg.jpg'
+import clouds from '../static/clouds-bg.jpg'
+import drizzle from '../static/drizzle-bg.jpg'
+import dust from '../static/dust-bg.jpg'
+import fog from '../static/fog-bg.jpg'
+import haze from '../static/haze-bg.jpg'
+import mist from '../static/mist-bg.jpg'
+import rain from '../static/rain-bg.jpg'
+import sand from '../static/sand-bg.jpg'
+import smoke from '../static/smoke-bg.jpg'
+import snow from '../static/snow-bg.jpg'
+import squall from '../static/squall-bg.jpg'
+import thunderstorm from '../static/thunderstorm-bg.jpg'
+import tornado from '../static/tornado-bg.jpg'
+
+const images = {
+    'ash': ash,
+    'clear': clear,
+    'clouds': clouds,
+    'drizzle': drizzle,
+    'dust': dust,
+    'fog': fog,
+    'haze': haze,
+    'mist': mist, 
+    'rain': rain,
+    'sand': sand,
+    'smoke': smoke,
+    'snow': snow,
+    'squall': squall,
+    'thunderstorm': thunderstorm,
+    'tornado': tornado
+};
+
+export default images;
